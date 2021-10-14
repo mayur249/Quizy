b3cd3b5e135a72f8f330cbeed07100e252b44fa5
mayur249 <kundermayuresh249@gmail.com>
2021-10-14 10:46:23 +0530

    fetching data from api successfull

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Footer, Header, Home, Quiz, Result } from "./components";
 
 function App() {
+  const [name, setName] = useState("initialState");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    console.log("DATA", data);
+  };
+
   return (
     <BrowserRouter>
       <div
@@ -12,10 +28,20 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" exact>
-            <Home />
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
           </Route>
           <Route path="/quiz" exact>
-            <Quiz />
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
           </Route>
           <Route path="/result" exact>
             <Result />
